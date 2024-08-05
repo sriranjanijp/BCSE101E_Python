26f2b6df78de8bdf5e2c280b938859f060fdb5ee
sriranjanijp <sriranjani.jp@gmail.com>
2024-08-05 21:23:22 +0530

    05.08.24

---
+++ area_of_rectangle.py
@@ -0,0 +1,12 @@
+print("Enter your first name")
+    #inputting strings
+name = input()
+last = input("Enter your last name\n")
+print("Enter length of rectangle")
+length = int(input())
+breadth = int(input("Enter breadth of rectangle\n"))
+area = length*breadth
+    #printing the result
+print("The area of the rectangle is:",area)
+    #tab
+print(name,"\t",last)
