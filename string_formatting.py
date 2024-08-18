31b598f89778de8d85fc9a24a1d1bc08e37869c5
sriranjanijp <sriranjani.jp@gmail.com>
2024-08-18 11:22:10 +0530

    "18.08.24"

---
+++ string_formatting.py
@@ -0,0 +1,15 @@
+first_name = "ADA"
+last_name = "LOVELACE"
+name = f"{first_name} {last_name}"
+name = name.title()
+print(name)
+print(name.upper())
+print(name.lower())
+message = f"Hello! {name.lower()}"
+print(message)
+print("Hello, Welcome to python\n\tWhat's your name\t?")
+
+fav_language = "  Python  "
+print(fav_language.rstrip())
+print(fav_language.lstrip(),end='')
+print(fav_language.strip())
