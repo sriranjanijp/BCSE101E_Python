11b0a61b8305d4b67dd5da13463ffcb8d90d444a
sriranjanijp <sriranjani.jp@gmail.com>
2024-08-18 16:23:05 +0530

    18.08.24

--- conditionals.py
+++ conditionals.py
@@ -2,4 +2,11 @@ car = 'subaru'
 print("Is car == 'subaru'? I predict True.") 
 print(car == 'subaru') 
 print("\nIs car == 'audi'? I predict False.") 
-print(car == 'bmw')
+print(car == 'audi')
+car = 'SUBARU'
+print("Is the car a subaru?")
+print(car.lower()=='subaru')
+
+age = 8
+if age<18:
+    print(f"You are {age}. You are not eligible to vote")
