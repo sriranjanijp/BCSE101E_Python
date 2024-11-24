9b1bd5a70e10d31c558f6764daf2bc58ab633627
sriranjanijp <sriranjani.jp@gmail.com>
2024-11-24 10:23:21 +0530

    People - AgustD

---
+++ FAT_Practice/binary_search.py
@@ -0,0 +1,17 @@
+listin = [1,2,3,4,5,6,7,8,9,10]
+e = 8
+c = 0
+low = 0
+high = len(listin)-1
+while low<=high:
+    mid = low+(high-low)//2
+    if listin[mid]<e:
+        low = mid+1
+    elif listin[mid]>e:
+        high = mid-1
+    else:
+        print(f"Element found at index {mid}")
+        c = 1
+        break
+if c==0:
+    print("Element not found")
