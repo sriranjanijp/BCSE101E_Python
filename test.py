1be560b11991003fe24d11f4a0976b53a7e38837
sriranjanijp <sriranjani.jp@gmail.com>
2024-11-24 11:32:49 +0530

    Safe Zone - J Hope

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-lista = ['q','w','e']
-lista.remove('e')
-print (lista)
+d = {"apple": 10,
+    "banana": 20,
+    "cherry": 15,
+    "date": 30,
+    "fig": 25}
+my = 'fig'
+a = d.get(my,"Key not found")
+print(list(d.keys()))
